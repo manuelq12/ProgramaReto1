92d56bee091b6ae270908d27440b069bf3937826
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-08-11 17:52:17 -0500

    interfaz principal

---
+++ Libreria/Libreria/interfazPrincipal.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Libreria
+{
+    partial class interfazPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Administrador = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Libreria.Properties.Resources.logo_icesi;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(220, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 106);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agendar Libro";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Administrador
+            // 
+            this.Administrador.Location = new System.Drawing.Point(142, 273);
+            this.Administrador.Name = "Administrador";
+            this.Administrador.Size = new System.Drawing.Size(184, 59);
+            this.Administrador.TabIndex = 2;
+            this.Administrador.Text = "Administrador";
+            this.Administrador.UseVisualStyleBackColor = true;
+            this.Administrador.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // interfazPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Libreria.Properties.Resources.FONDO;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Administrador);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "interfazPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Administrador;
+    }
+}
---
+++ Libreria/Libreria/interfazPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
+{
+    public partial class interfazPrincipal : Form
+    {
+        public interfazPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form ventAgenda = new interfazAgendar();
+            ventAgenda.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Form ventAdmin = new interfazAdmin();
+            ventAdmin.Show();
+
+        }
+    }
+}
