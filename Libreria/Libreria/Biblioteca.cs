fc9fd356b1863046a0de1ef22719d6cf74761f42
manuelq12 <manuelq12@users.noreply.github.com>
2018-08-11 15:38:52 -0500

    Agrego Clases con atributos y métodos

---
+++ Libreria/Libreria/Biblioteca.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    class Biblioteca
+    {
+        private Libro[] libros;
+        private int index;
+
+        public Biblioteca(int tamanho)
+        {
+            this.libros = new Libro[tamanho];
+            Index = 0;
+        }
+
+        public Libro[] Libros { get => libros; set => libros = value; }
+        public int Index { get => index; set => index = value; }
+
+        public void agregarLibro(String titulo, String autor, String anho, int edicion)
+        {
+            if (index <= this.libros.Length - 1)
+            {
+                Libro agregar = new Libro(titulo, autor, anho, edicion, this.index);
+                this.libros[this.index] = agregar;
+                index++;
+            }
+
+        }
+        public void eliminarLibro(int index)
+        {
+            if (index > -1 && index < libros.Length)
+            {
+                libros[index] = null;
+            }
+        }
+        public int buscarLibro(String titulo)
+        {
+            int resultado = -1;
+            foreach (Libro s in libros)
+                if (s.Titulo.Equals(titulo))
+                {
+                    resultado = s.Index;
+                }
+
+            return resultado;
+
+        }
+    }
+
+}
---
+++ Libreria/Libreria/Libro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
+{
+    public class Libro
+    {
+        private String titulo;
+        private String autor;
+        private String anho;
+        private int edicion;
+        private int index;
+
+        public Libro(String pTitulo, String pAutor, String pAño, int pEdicion, int pIndex)
+        {
+            titulo = pTitulo;
+            autor = pAutor;
+            anho = pAño;
+            edicion = pEdicion;
+            index = pIndex;
+        }
+
+        public String Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+            set
+            {
+                titulo = value;
+            }
+        }
+        public String Autor
+        {
+            get
+            {
+                return autor;
+            }
+            set
+            {
+                autor = value;
+            }
+        }
+        public String Anho
+        {
+            get
+            {
+                return anho;
+            }
+            set
+            {
+                anho = value;
+            }
+        }
+        public int Edicion
+        {
+            get
+            {
+                return edicion;
+            }
+            set
+            {
+                edicion = value;
+            }
+        }
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        public String toString()
+        {
+            return "[ " + titulo + ", " + autor + ", " + anho + ", " + edicion + " ]";
+        }
+
+    }
+
+}
