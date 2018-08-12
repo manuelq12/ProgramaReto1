6de50538510545c04044863cf3861a56e8e179ed
JuanGallooo <36927348+JuanGallooo@users.noreply.github.com>
2018-08-11 19:00:13 -0500

    Algunas cosas

--- Libreria/Libreria/Biblioteca.cs
+++ Libreria/Libreria/Biblioteca.cs
@@ -11,8 +11,14 @@ namespace Libreria
         private Libro[] libros;
         private int index;
 
+        public List<Libro> LibrosFisicos { get; set; }
+        public List<Libro> LibrosOnline { get; set; }
+
         public Biblioteca(int tamanho)
         {
+            LibrosFisicos = new List<Libro>(tamanho);
+            LibrosOnline = new List<Libro>(tamanho);
+
             this.libros = new Libro[tamanho];
             Index = 0;
         }
@@ -48,6 +54,43 @@ namespace Libreria
 
             return resultado;
 
+        }
+
+        //Desde aqui 
+
+        public bool agregarLibroFisico(String titulo, String autor, String anho, int edicion, String tipo)
+        {
+            bool retorno = true;
+            foreach (Libro b in LibrosFisicos)
+                if (titulo.Equals(b.Titulo)) throw new Exception("Ya existe dicho libro fisico");
+            if (LibrosFisicos.Capacity == LibrosFisicos.Count) throw new Exception("Lista llena");
+            Libro a = new Libro(titulo, autor, anho, edicion, 0, tipo);
+            LibrosFisicos.Add(a);
+            return retorno;
+        }
+        public bool agregarLibroDigital(String titulo, String autor, String anho, int edicion, String tipo)
+        {
+            bool retorno = true;
+            foreach (Libro b in LibrosOnline)
+                if (titulo.Equals(b.Titulo)) throw new Exception("Ya existe dicho libro fisico");
+            if (LibrosOnline.Capacity == LibrosOnline.Count) throw new Exception("Lista llena");
+            Libro a = new Libro(titulo, autor, anho, edicion, 0, tipo);
+            LibrosOnline.Add(a);
+            return retorno;
+        }
+        public Libro buscarLibroFisico(String nombre)
+        {
+            Libro buscar = null;
+            foreach (Libro b in LibrosFisicos)
+                if (nombre.Equals(b.Titulo)) buscar = b;
+            return buscar;
+        }
+        public Libro buscarLibroOnline(String nombre)
+        {
+            Libro buscar = null;
+            foreach (Libro b in LibrosOnline)
+                if (nombre.Equals(b.Titulo)) buscar = b;
+            return buscar;
         }
     }
 
