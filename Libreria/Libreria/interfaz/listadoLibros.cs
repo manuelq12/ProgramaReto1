f6d5ab45ce578eecb7025af2947f9e5b1d3ef83a
manuelq12 <manuelq12@users.noreply.github.com>
2018-08-12 23:57:15 -0500

    Actualizar

--- Libreria/Libreria/interfaz/listadoLibros.cs
+++ Libreria/Libreria/interfaz/listadoLibros.cs
@@ -111,11 +111,45 @@ namespace Libreria
 
         private void butActualizar_Click(object sender, EventArgs e)
         {
-            string nombre = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Nombre (Si no desea hacerlo dejar en blanco dar Aceptar) :");
-            string autor = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Autor(Si no desea hacerlo dejar en blanco dar Aceptar):");
-            string anho = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Anho (Si no desea hacerlo dejar en blanco dar Aceptar):");
-            
+            string nombre = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Nombre (Si no desea hacerlo escriba NULL y de Aceptar) :");
+            string autor = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Autor(Si no desea hacerlo escriba NULL y de Aceptar):");
+            string anho = Microsoft.VisualBasic.Interaction.InputBox("Cambiar Anho (Si no desea hacerlo escriba NULL y de Aceptar):");
+            int index = tabla.CurrentRow.Index;
+            Libro p = null;
+            int bas = 0;
+            if(butEliminarDigitales.Enabled == false){
+                p = principal.BuscarEnTablaFisicos(index);
+                bas = 1;
+              }
+            else
+            {
+                p = principal.BuscarEnTablaDigitales(index);
+                bas = 2;
+            }
+            principal.EliminarLibros(p.Titulo, p.Tipo);
+            if (!nombre.Equals("NULL"))
+            {
+                p.Titulo = nombre;
+            }
+            if (!autor.Equals("NULL"))
+            {
+                p.Autor = autor;
+            }
+            if (!anho.Equals("NULL"))
+            {
+                p.Anho = anho;
+            }
+            principal.AgregarLibros(p.Titulo, p.Autor, p.Anho, p.Tipo);
 
+            if(bas == 1) {
+                refrescarFisicos();
+            }
+            else
+            {
+                refrescarDigitales();
+            }
+
+        
         }
     }
 }
